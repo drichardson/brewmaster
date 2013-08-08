44ee4fdc42d280044c26905b5187adcc21c244c5
Doug Richardson <dougie.richardson@gmail.com>
2013-08-07 18:19:11 -0700

    ddplatform initial checkin. very broken.

---
+++ ddplatform/objects/DDException.h
@@ -0,0 +1,7 @@
+#ifndef COEXCEPTION_H_
+#define COEXCEPTION_H_
+
+// Logs and exception and aborts.
+void COException(const char* msg, ...);
+
+#endif
---
+++ ddplatform/objects/DDObjects.h
@@ -0,0 +1,14 @@
+//
+//  CObjects.h
+//  CObjects
+//
+//  Created by Doug Richardson on 1/8/13.
+//  Copyright (c) 2013 Doug Richardson. All rights reserved.
+//
+
+#ifndef CObjects_CObjects_h
+#define CObjects_CObjects_h
+
+#include "DDObjectBase.h"
+
+#endif
