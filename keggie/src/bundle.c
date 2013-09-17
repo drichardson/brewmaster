24e0d732e19da2f853473d6acf26137337b3728c
Doug Richardson <dougie.richardson@gmail.com>
2013-09-17 01:39:44 +0000

    fix bundle_path bug due to readlink not null terminating string.

--- keggie/src/bundle.c
+++ keggie/src/bundle.c
@@ -6,15 +6,23 @@
 #include <libgen.h>
 #include <limits.h>
 #include <stdio.h>
+#include <assert.h>
 
 void bundle_path(char* path, size_t max_path) {
-    if (readlink("/proc/self/exe", path, max_path) != -1) {
-       dirname(path);
-       size_t len = strlen(path);
-       if (len+1 < max_path) {
-           path[len] = '/';
-           path[len+1] = 0;
-       }
+    if (max_path == 0) return;
+
+    int rc = readlink("/proc/self/exe", path, max_path-1);
+    if(rc != -1) {
+        // readlink doesn't insert a 0 byte at the end.
+        path[rc] = 0;
+        dirname(path);
+        size_t len = strlen(path);
+        if (len+1 < max_path) {
+            path[len] = '/';
+            path[len+1] = 0;
+        } else {
+            log_error("bundle_path: couldn't append trailing slash because max_path = %d isn't big enough.", max_path);
+        }
     } else {
         log_error("bundle_path: couldn't get path to current bundle. %d: %s", errno, strerror(errno));
     }
