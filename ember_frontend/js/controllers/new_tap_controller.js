1f49cda4a9ee315f21d51d60a6c165284c096385
Doug Richardson <dougie.richardson@gmail.com>
2013-11-10 10:01:40 -0800

    Remove old ember frontend

--- ember_frontend/js/controllers/new_tap_controller.js
+++
@@ -1,7 +0,0 @@
-var EditTapController = require('./edit_tap_controller');
-
-// inherit from edit controller
-var NewTapController = EditTapController.extend();
-
-module.exports = NewTapController;
-
